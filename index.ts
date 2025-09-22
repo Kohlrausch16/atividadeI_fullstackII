f6ef52f5460ec0c453e64ad3e9713a76c6c34a0e
Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-09-22 17:37:30 -0300

    CORS - fix

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import express, { json } from "express";
 import productRouter from "./src/Routes/ProductRoutes";
 import userRouter from "./src/Routes/UserRoutes";
+import cors from 'cors';
 
 const server = express();
 
+const corsOptions = {origin: 'hhtp://localhost:3000'}
+
+server.use(cors(corsOptions));
 server.use(express.urlencoded({extended: true}));
 server.use(express.json());
 
 server.use(productRouter, userRouter);
-server.listen(3000);
+server.listen(4000);
